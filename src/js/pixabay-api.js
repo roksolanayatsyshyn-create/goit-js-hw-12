45000dacb2cb919b1023ec48337716e89386e79d
Roksolana Fedyna <roksolanayatsyshyn@gmail.com>
2025-11-25 22:45:56 +0200

    v3

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,31 +1,33 @@
 import axios from "axios";
-import iziToast from "izitoast";
-import "izitoast/dist/css/iziToast.min.css";
 
 axios.defaults.baseURL = "https://pixabay.com/api/";
-const API_KEY="53351360-25752520852e3bd129b79410f";
+const API_KEY = "53351360-25752520852e3bd129b79410f";
 
-export function getImagesByQuery(query,page=1,perPage=15){
-return axios.get("",{params:{
-  key:API_KEY,
-  q:query,
-  image_type: "photo",
-  orientation: "horizontal",
-  safesearch: true,
-  page,
-  per_page:perPage,
-},})
-  .then(response=>{
-    if (!response.data || response.data.hits.length === 0) {
-  iziToast.warning({
-    message: 'На жаль, за вашим запитом зображень не знайдено. Спробуйте ще раз!',
-    position: 'topRight',
-  });
-  return { hits: [] };
+export async function getImagesByQuery(query, page = 1, perPage = 15) {
+try {
+const response = await axios.get("", {
+params: {
+key: API_KEY,
+q: query,
+image_type: "photo",
+orientation: "horizontal",
+safesearch: true,
+page,
+per_page: perPage,
+},
+});
+
+if (!response.data || !response.data.hits || response.data.hits.length === 0) {
+  return { hits: [], totalHits: 0 };
 }
 
-      return response.data;
-    })
+return {
+  hits: response.data.hits,
+  totalHits: response.data.totalHits,
+};
 
-    .catch(err => {console.error(err);
-    return { hits: [] }})}
+} catch (err) {
+console.error(err);
+return { hits: [], totalHits: 0 };
+}
+}
